47dac7f90905a4b3ee11b851775ba740379b3bfa
dalinar42 <nelsonboy42@gmail.com>
2020-01-16 16:19:25 -0700

    hopefully fix the php

--- web/shared/heading.php
+++ web/shared/heading.php
@@ -1,6 +1,4 @@
-<?php
-echo '<div class="jumbotron text-center bg-dark text-white">
+<div class="jumbotron text-center bg-dark text-white">
       <h1>Austin Nelson</h1>
       <p>Software Engineering</p>
-      </div>';
-?>
+</div>;
