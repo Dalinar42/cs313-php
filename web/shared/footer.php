7b3d920adf7b395221f4f2bf90077c780408acd4
dalinar42 <nelsonboy42@gmail.com>
2020-01-18 18:29:11 -0700

    fix footer for real

--- web/shared/footer.php
+++ web/shared/footer.php
@@ -1,7 +1,7 @@
-<div class="container-fluid pt-4 text-center bg-dark">
+<div class="container-fluid pt-3 bg-dark text-white">
    <?php
       date_default_timezone_set("America/Denver");
       echo "Date: " . date("m/d/Y") . "</br>";
-      echo "Time: " . date("h:i:sa") . "</br>";
+      echo "Time: " . date("h:i a") . "</br>";
    ?>
 </div>
