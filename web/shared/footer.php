2909c45cfb704295fd8000c6556d8fdb93cf41a8
dalinar42 <nelsonboy42@gmail.com>
2020-01-18 18:14:29 -0700

    fix time in footer

--- web/shared/footer.php
+++ web/shared/footer.php
@@ -1,6 +1,7 @@
-<div class="container-fluid pt-4 text-center bg-secondary">
+<div class="container-fluid pt-4 text-center bg-dark">
    <?php
-      echo "Date: " . date(m/d/Y) . "</br>";
+      date_default_timezone_set("America/Denver");
+      echo "Date: " . date("m/d/Y") . "</br>";
       echo "Time: " . date("h:i:sa") . "</br>";
    ?>
 </div>
