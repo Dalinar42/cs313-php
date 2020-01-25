9b98cb2c73e5d376345baf82d8c8f091829a1614
dalinar42 <nelsonboy42@gmail.com>
2020-01-25 13:17:19 -0700

    fix index in root, hopefully

--- web/shared/menu.php
+++ web/shared/menu.php
@@ -2,10 +2,10 @@
    <div class="collapse navbar-collapse">
       <ul class="navbar-nav mr-auto">
          <li class="nav-item active">
-            <a class="nav-link text-light" href="/index.php">Home<span class="sr-only">(current)</span></a>
+            <a class="nav-link text-light" href="../index.php">Home<span class="sr-only">(current)</span></a>
          </li>
          <li class="nav-item active">
-            <a class="nav-link text-light" href="/web/assignments/assignments.php">Assignments</a>
+            <a class="nav-link text-light" href="../assignments/assignments.php">Assignments</a>
          </li>
       </ul>
    </div>
