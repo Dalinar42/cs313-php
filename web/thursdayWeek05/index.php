1a56ce082c382e7151cd17730d4a6b4d13c3beea
dalinar42 <nelsonboy42@gmail.com>
2020-02-06 16:36:36 -0700

    index is updated, query's are updated

--- web/thursdayWeek05/index.php
+++ web/thursdayWeek05/index.php
@@ -1,3 +1,42 @@
-<?php 
+<?php
+   function getDB() {
+      $db = NULL;
+      try
+      {
+      $dbUrl = getenv('DATABASE_URL');
+
+      $dbOpts = parse_url($dbUrl);
+
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      catch (PDOException $ex)
+      {
+         echo 'Error!: ' . $ex->getMessage();
+         die();
+      }
+      return $db;
+   }
+
+   $db = getDb();
+
+   $event = $db->prepare("SELECT * FROM w5_event");
+   $event->execute();
+   while ($fRow = $event->fetch(PDO::FETCH_ASSOC))
+   {
+      $name = $fRow["name"];
+      $image = $fRow["image"];
+      $id = $fRow['id'];
+      echo "<img src=\"$image\" alt=\"$name\">";
+      echo $name;
+   }
+
 
 ?>
--- web/thursdayWeek05/query.sql
+++ web/thursdayWeek05/query.sql
@@ -1,29 +1,61 @@
 \echo '2a. How many events are there?'
 ---------------- Your code here ----------------
+SELECT COUNT(*) FROM w5_event;
 
--- \echo '2b. How many participants are there?'
+\echo '2b. How many participants are there?'
 ---------------- Your code here ----------------
+SELECT COUNT(*) FROM w5_participant;
 
--- \echo '3a. What is the third event when sorted alphabetically (by name)? '
+\echo '3a. What is the third event when sorted alphabetically (by name)? '
 ---------------- Your code here ----------------
+SELECT name FROM w5_event
+ORDER BY name
+LIMIT 1 OFFSET 2;
 
--- \echo '3b. What is the third event when sorted by ID? '
+\echo '3b. What is the third event when sorted by ID? '
 ---------------- Your code here ----------------
+SELECT name FROM w5_event
+ORDER BY id
+LIMIT 1 OFFSET 2;
 
--- \echo '4a. List the names alphabetically of all the participants in the ''Toughman Utah'' competition'
+\echo '4a. List the names alphabetically of all the participants in the ''Toughman Utah'' competition'
 ---------------- Your code here ----------------
+SELECT DISTINCT p.name FROM w5_event e
+JOIN w5_event_participant ep ON e.id = ep.event_id
+JOIN w5_participant p ON ep.participant_id = p.id
+WHERE e.name = 'Toughman Utah'
+ORDER BY p.name;
 
--- \echo '4b. List the names (sorted by id) of all the participants in the ''Kauai Marathon'' competition'
+\echo '4b. List the names (sorted by id) of all the participants in the ''Kauai Marathon'' competition'
 ---------------- Your code here ----------------
+SELECT DISTINCT p.id, p.name FROM w5_event e
+JOIN w5_event_participant ep ON e.id = ep.event_id
+JOIN w5_participant p ON ep.participant_id = p.id
+WHERE e.name = 'Kauai Marathon'
+ORDER BY p.id;
 
--- \echo '5a. How many competitions has ''Black Panther'' competed in?'
+\echo '5a. How many competitions has ''Black Panther'' competed in?'
 ---------------- Your code here ----------------
+SELECT COUNT(e.name) FROM w5_event e
+JOIN w5_event_participant ep ON e.id = ep.event_id
+JOIN w5_participant p ON ep.participant_id = p.id
+WHERE p.name = 'Black Panther';
 
--- \echo '5b. How can you verify that your count is correct?'  
+\echo '5b. How can you verify that your count is correct?'
 ---------------- Your code here ----------------
+SELECT p.id, p.name FROM w5_participant p
+WHERE p.name = 'Black Panther';
+SELECT ep.id, ep.event_id, ep.participant_id FROM w5_event_participant ep
+WHERE ep.participant_id = 32;
 
--- \echo '5c. Who has competed in the most competitions? How many have they competed in?'
+\echo '5c. Who has competed in the most competitions? How many have they competed in?'
 ---------------- Your code here ----------------
+SELECT p.name, COUNT(*) as c FROM w5_event e
+JOIN w5_event_participant ep ON e.id = ep.event_id
+JOIN w5_participant p ON ep.participant_id = p.id
+GROUP BY p.name
+ORDER BY c DESC
+LIMIT 1 OFFSET 0;
 
 -- \echo '5d. Who has competed in the least competitions? How many have they competed in?'
 ---------------- Your code here ----------------
