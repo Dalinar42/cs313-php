9a38add78336248f01b31cf12b98983e7293b458
dalinar42 <nelsonboy42@gmail.com>
2020-02-08 18:24:09 -0700

    update to display some games the index.php file in videoGameDB

--- web/videoGameDB/index.php
+++ web/videoGameDB/index.php
@@ -16,14 +16,40 @@
    require 'dbConnect.php';
    $db = getDb();
 ?>
-
+   <div class="jumbotron jumbotron-fluid text-center bg-dark text-white">
+      <h1>Assignments</h1>
+      <span>All my hard work</span>
+   </div>
+   <?php include '../shared/menu.php'; ?>
 </header>
 
-<div class="container-fluid">
+
+<table class="table table-bordered container">
+   <thead class="thead-dark">
+      <tr>
+         <th scope="col">ID</th>
+         <th scope="col">Name</th>
+      </tr>
+   </thead>
+   <tbody>
+      <?php
+         $event = $db->prepare("SELECT * FROM game_list");
+         $event->execute();
+         while ($fRow = $event->fetch(PDO::FETCH_ASSOC))
+         {
+            $game_name = $fRow["game_name"];
+            $id = $fRow['id'];
+            echo "<tr><td>$id</td><td>$game_name</td></tr>";
+         }
+      ?>
+   </tbody>
+</table>
+
+<!-- <div class="container-fluid">
    <form action="">
       <input type="text">
    </form>
-</div>
+</div> -->
 
 
 <footer class="footer">
