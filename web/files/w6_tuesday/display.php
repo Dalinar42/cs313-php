33f658cb071837ca320ae3e5cfe011f50f022eda
dalinar42 <nelsonboy42@gmail.com>
2020-02-11 15:45:00 -0700

    update display.php

--- web/files/w6_tuesday/display.php
+++ web/files/w6_tuesday/display.php
@@ -6,7 +6,25 @@
 		<div class="container">
          <?php
             // retrieve url parameter
+            $personId = $_GET['personId'];
             // execute query to pull up data from that id
+            $statement = $db->prepare('SELECT * FROM w6_user WHERE ID = :personId');
+            $statement->bindvalue(':personId', $personId);
+            $statement->execute();
+            while ($row = $statement0<fetch(PDO::FETCH_ASSOC))
+            {
+               $id = $row['id'];
+               $first = $row['first_name'];
+               $last = $row['last_name'];
+               $food = $row['food_typ'];
+
+               $statement = $db->prepare('SELECT food FROM w6_food WHERE ID = $food_id');
+               $statement->execute();
+               while ($frow = $statement0<fetch(PDO::FETCH_ASSOC))
+               {
+                  $food = $frow['food'];
+               }
+            }
             // execute another query to get food data
             // display name and favorite food
          ?>
