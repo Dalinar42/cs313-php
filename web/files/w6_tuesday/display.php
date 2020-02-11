508abd9d5c4f672792e1f261758f46d6aaa5fa65
dalinar42 <nelsonboy42@gmail.com>
2020-02-11 16:00:08 -0700

    fix bug display.php

--- web/files/w6_tuesday/display.php
+++ web/files/w6_tuesday/display.php
@@ -8,8 +8,9 @@
             // retrieve url parameter
             $personId = $_GET['personId'];
             // execute query to pull up data from that id
-            $statement = $db->prepare('SELECT * FROM w6_user WHERE ID = :personId');
-            $statement->bindvalue(':personId', $personId);
+            // single quotes used for bind
+            $statement = $db->prepare('SELECT * FROM w6_user WHERE id = :personId');
+            $statement->bindValue(':personId', $personId);
             $statement->execute();
             while ($row = $statement->fetch(PDO::FETCH_ASSOC))
             {
@@ -18,14 +19,15 @@
                $last = $row['last_name'];
                $food_id = $row['food_type'];
 
-               $fstatement = $db->prepare("SELECT food FROM w6_food WHERE ID = $food_id");
+               // double quotes used without bind
+               $fstatement = $db->prepare("SELECT food FROM w6_food WHERE id = $food_id");
                $fstatement->execute();
                while ($frow = $fstatement->fetch(PDO::FETCH_ASSOC))
                {
                   $food = $frow['food'];
                }
             }
-            echo "<h1>$first $last's favorite food is $ffood</h1>";
+            echo "<h1>$first $last's favorite food is $food</h1>";
             // execute another query to get food data
             // display name and favorite food
          ?>
