5d18fe58bbdeeae06892c6743f21c1250383948c
dalinar42 <nelsonboy42@gmail.com>
2020-02-14 15:56:12 -0700

    revert to include genre table again

--- db/create.sql
+++ db/create.sql
@@ -3,16 +3,21 @@ CREATE TABLE users
 , username    VARCHAR (50)  NOT NULL
 , firstname   VARCHAR (50)
 , lastname    VARCHAR (50)
-, PASSWORD    VARCHAR (50)
 , phone       VARCHAR (100)
 , email       VARCHAR (100) NOT NULL
 , infohidden  VARCHAR (1)   NOT NULL
+);
+
+CREATE TABLE genres
+( id          SERIAL        NOT NULL PRIMARY KEY
+, genrename   VARCHAR (100) NOT NULL
 );
 
 CREATE TABLE game_list
 ( id          SERIAL        NOT NULL PRIMARY KEY
 , userid      INT           NOT NULL REFERENCES users(id)
 , gamename    VARCHAR (100) NOT NULL
+, genreid     int           NOT NULL REFERENCES genres(id)
 , dateadded   VARCHAR (100) NOT NULL
 , tags        VARCHAR (100)
 );
@@ -23,9 +28,8 @@ CREATE TABLE wish_list
 , gameid      INT           NOT NULL REFERENCES game_list(id)
 );
 
--- Stretch
--- CREATE TABLE friends_list
--- ( id          SERIAL        NOT NULL PRIMARY KEY
--- , userid      INT           NOT NULL REFERENCES users(id)
--- , friendid    INT           NOT NULL REFERENCES users(id)
--- );
+CREATE TABLE friends_list
+( id          SERIAL        NOT NULL PRIMARY KEY
+, userid      INT           NOT NULL REFERENCES users(id)
+, friendid    INT           NOT NULL REFERENCES users(id)
+)
