f7aea7569b26f350e930a25a71f6afed72f5ee08
dalinar42 <nelsonboy42@gmail.com>
2020-02-15 11:26:09 -0700

    debugging insert.php

--- web/videoGameDB/insert.php
+++ web/videoGameDB/insert.php
@@ -7,17 +7,17 @@ $gname = $_POST['gamename'];
 $gsId = $_POST['genreid'];
 $id = $_SESSION['userid'];
 $datetoday = date("m/d/Y");
-echo "Game name: " . $gname;
-echo " Genre Id: " . $gsId;
-echo " User Id: " . $id;
-echo " Date Added: " . $datetoday;
+
+// echo "Game name: " . $gname;
+// echo " Genre Id: " . $gsId;
+// echo " User Id: " . $id;
+// echo " Date Added: " . $datetoday;
+
 require("dbConnect.php");
-$db = get_db();
-echo " after the db connect ";
+$db = getDB();
 
 try
 {
-	echo "Getting to the query";
 	$query = 'INSERT INTO game_list ( gamename, genreid, userid, dateadded) VALUES (:gname, :gsId, :id, :datetoday)';
 	$statement = $db->prepare($query);
 	$statement->bindValue(':gname', $gname);
@@ -25,7 +25,6 @@ try
 	$statement->bindValue(':id', $id);
 	$statement->bindValue(':datetoday', $datetoday);
 	$statement->execute();
-	echo "Does it break here?";
 
 	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
 	// get id of last inserted row - save in $userId
@@ -33,15 +32,11 @@ try
 }
 catch (Exception $ex)
 {
-	echo "What about here?";
 	echo "Error with DB. Details: $ex";
 	die();
-	echo "Does it die in the catch?";
 }
 // header("Location: display.php/?personId=$userId");
 header("Location: profile.php/?personId=$id");
-
-echo "Do we get past the redirect?";
 
 die();
 ?>
