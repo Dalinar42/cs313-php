c2a2af85d926dc8cf1b9ce2be4b16cbd8cf1b3f3
dalinar42 <nelsonboy42@gmail.com>
2020-02-15 12:12:41 -0700

    adding the remove function

--- web/videoGameDB/remove.php
+++ web/videoGameDB/remove.php
@@ -3,18 +3,17 @@ session_start();
 
 // INSERT INTO game_list ( gamename, genreid, userid, dateadded)
 // 	VALUES ( 'StarCraft', 1, 1, '2/8/2020' );
-$gname = $_POST['gameid'];
+$gid = $_POST['gameid'];
 $id = $_SESSION['userid'];
-$datetoday = date("m/d/Y");
 
 require("dbConnect.php");
 $db = getDB();
 
 try
 {
-	$query = 'DELETE FROM game_list WHERE id = :gameid; ';
+	$query = 'DELETE FROM game_list WHERE id = :gid; ';
 	$statement = $db->prepare($query);
-	$statement->bindValue(':gameid', $gameid);
+	$statement->bindValue(':gid', $gid);
 	$statement->execute();
 
 	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
