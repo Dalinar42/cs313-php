36219ef80fd00baaf2d96f3017bdcd829b738329
dalinar42 <nelsonboy42@gmail.com>
2020-02-20 15:49:58 -0700

    ta07

--- web/files/teamActivities/ta07/post_user.php
+++ web/files/teamActivities/ta07/post_user.php
@@ -1,18 +1,23 @@
 <?php
-    session_start();
-    require 'dbconnect.php';
-    $db = get_db();
-    $url = 'login.php';
+   session_start();
+   require 'dbconnect.php';
+   $db = get_db();
+   $url = 'login.php';
+   $varName = "/^[A-Z][a-zA-Z ]+$/";
+   $number = "/^[0-9]+$/";
+   $name = $_POST['username'];
+   $pass = $_POST['pass'];
 
-    $name = $_POST['username'];
-    $pass = $_POST['pass'];
 
+   echo "username = $name\n";
+   echo "pass = $pass\n";
+   echo password_hash($name, PASSWORD_DEFAULT);
 
-    echo 'username = $name';
-    echo '$pass = $pass';
-    echo password_hash($name, PASSWORD_DEFAULT);
+   $statement = $db->prepare("INSERT INTO ta07_user (username, userpassword) VALUES(:name, :pass)");
+   $statement->bindValue(':name', $name);
+	$statement->bindValue(':pass', $pass);
+   $statement->execute();
 
-    //check if all fill in:
-
-    //header('Location: ' . $url);
+   header('Location: ' . $url);
+   die();
 ?>
