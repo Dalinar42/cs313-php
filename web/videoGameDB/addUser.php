01c48e8465a95282e3322f8fed5dd5f683c18d8b
dalinar42 <nelsonboy42@gmail.com>
2020-02-21 17:53:56 -0700

    adding some functionality to creating new users

--- web/videoGameDB/addUser.php
+++ web/videoGameDB/addUser.php
@@ -14,7 +14,7 @@
    $retrieveUsername = $db->prepare("SELECT username FROM users WHERE username = :username");
    $retrieveUsername->bindValue(':username', $username);
    $retrieveUsername->execute();
-
+   echo 1;
    $usernameRow = $retrieveUsername->fetch(PDO::FETCH_ASSOC);
    if (isset($usernameRow['username'])) {
       $_SESSION['errorStr'] = "Username already taken";
@@ -22,11 +22,11 @@
       header('Location: ' . $url);
       die();
    }
-
+   echo 2;
    $retrieveEmail = $db->prepare("SELECT email FROM users WHERE email = :email");
    $retrieveEmail->bindValue(':email', $email);
    $retrieveEmail->execute();
-
+   echo 3;
    $emailRow = $retrieveEmail->fetch(PDO::FETCH_ASSOC);
    if (isset($emailRow['email'])) {
       $_SESSION['errorStr'] = "User already exists with that email";
@@ -34,14 +34,14 @@
       header('Location: ' . $url);
       die();
    }
-
+   echo 4;
    $insertUser = $db->prepare("INSERT INTO users (username, email, userpassword) VALUES(:username, :email, :hashpassword)");
    $insertUser->bindValue(':username', $username);
    $insertUser->bindValue(':email', $email);
    $insertUser->bindValue(':hashpassword', $hashpassword);
    $insertUser->execute();
    $_SESSION['successStr'] = "Success! Please log in!";
-
+   echo 5;
    header('Location: ' . $url);
    die();
 ?>
